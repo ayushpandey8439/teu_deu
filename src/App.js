9258c1b485206bb4a6edf6a20d67f908a20918f8
Ayush Pandey <ayushpandey8439@gmail.com>
2021-03-08 18:02:25 +0100

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.scss';
+
+function App() {
+    return (
+        <div className="App">
+            <div className="Header">
+                <div className="Logo">TeuDeu</div>
+            </div>
+        </div>
+    );
+}
+
+
+export default App;
